65843a7d53dba9031be59c81b21edc0bcaf2ca6d
ciphrd <ciphrd@fxhash.xyz>
2025-03-25 14:39:38 +0100

    collisions tranfer of energy

--- src/physics/constraints/collisions.js
+++ src/physics/constraints/collisions.js
@@ -2,10 +2,11 @@ import { twoBodiesId } from "../../utils/cache"
 import { SpacePartition } from "../../opti/hash-partition"
 import { vec2 } from "../../utils/vec"
 
+const _v2a = vec2()
+
 export class Collisions {
   constructor(bodies) {
     this.update(bodies)
-    this.dir = vec2()
   }
 
   update(bodies) {
@@ -18,19 +19,28 @@ export class Collisions {
     const done = {}
     this.part.update()
 
-    let e, id, _
+    let e, s, imp, id, _
     for (const a of this.bodies) {
       for (const b of this.part.neighbours(a)) {
         id = twoBodiesId(a, b)
         if (a === b || done[id]) continue
         _ = computeCache.get(a, b)
-        if (_.d < 0.0000001) continue
+
+        if (_.d < 0.00001) continue
         e = _.d - (a.radius + b.radius)
         if (e < 0) {
           e /= 2
-          this.dir.copy(_.dir).mul(e)
-          a.pos.add(this.dir)
-          b.pos.sub(this.dir)
+          _v2a.copy(_.dir).mul(0.4 * e)
+          a.pos.add(_v2a)
+          b.pos.sub(_v2a)
+
+          _v2a.copy(b.vel).sub(a.vel)
+          s = _v2a.dot(_.dir)
+          if (s < 0) {
+            imp = _v2a.copy(_.dir).mul((-(1 + 0.9) * s) / 2)
+            a.vel.sub(imp)
+            b.vel.add(imp)
+          }
         }
         done[id] = true
       }
