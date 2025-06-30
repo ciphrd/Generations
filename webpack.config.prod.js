c8a50361426533fd184164711c8e6acebc195437
ciphrd <ciphrd@fxhash.xyz>
2025-06-30 18:12:29 +0200

    wip

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,2 +1,17 @@
 import withFxhashCli from "@fxhash/cli/webpack"
-export default withFxhashCli({ mode: "prd" }, {})
+export default withFxhashCli(
+  { mode: "prd" },
+  {
+    module: {
+      rules: [
+        {
+          test: /\.glsl$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "raw-loader",
+          },
+        },
+      ],
+    },
+  }
+)
